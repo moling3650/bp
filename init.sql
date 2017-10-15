de446c712d31f0d682ffd03690d55f8925682049
moling <365024424@qq.com>
2017-10-15 23:37:13 +0800

    初始化项目

---
+++ init.sql
@@ -0,0 +1,59 @@
+SET NAMES utf8;
+
+DROP DATABASE IF EXISTS bp;
+
+CREATE DATABASE bp;
+
+USE bp;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON bp.* TO 'bp'@'localhost' IDENTIFIED BY 'bp-data';
+
+DROP TABLE IF EXISTS B_Project;
+
+CREATE TABLE B_Project (
+    id int not null auto_increment comment '主键',
+    projrct_code varchar(255) not null comment '项目代码',
+    projrct_name varchar(255) not null comment '项目名称',
+    province varchar(255) not null comment '省份',
+    city varchar(255) not null comment '城市',
+    pm varchar(255) not null comment '项目负责人',
+    phone_no varchar(255) not null comment '手机号码',
+    create_date datetime not null default current_timestamp comment '创建时间',
+    remark varchar(255) default null comment '备注',
+    primary key(id),
+    unique(projrct_code)
+) ENGINE=innodb DEFAULT CHARSET=utf8;
+
+CREATE TABLE B_Building (
+    id int not null auto_increment comment '主键',
+    building_code varchar(255) not null comment '建筑代码',
+    building_name varchar(255) not null comment '建筑名称',
+    projrct_code varchar(255) not null comment '所属项目',
+    type int not null comment '建筑类型: 1: 楼塔|2: 桥梁|3: 隧道|4: 堤坝',
+    create_date datetime not null default current_timestamp comment '创建时间',
+    remark varchar(255) default null comment '备注',
+    primary key(id),
+    unique(building_code)
+) ENGINE=innodb DEFAULT CHARSET=utf8;
+
+CREATE TABLE B_Building_Unit (
+    id int not null auto_increment comment '主键',
+    unit_nmae varchar(255) not null comment '单元名称',
+    building_code varchar(255) not null comment '所属建筑',
+    create_date datetime not null default current_timestamp comment '创建时间',
+    remark varchar(255) default null comment '备注',
+    primary key(id)
+) ENGINE=innodb DEFAULT CHARSET=utf8;
+
+CREATE TABLE B_Upper_Monitor (
+    id int not null auto_increment comment '主键',
+    projrct_code varchar(255) not null comment '所属项目',
+    monitor_code varchar(255) not null comment 'MAC地址或其他唯一编号',
+    monitor_name varchar(255) not null comment '设备名称',
+    state int not null comment '状态',
+    channel_count int not null comment '通道数',
+    create_date datetime not null default current_timestamp comment '创建时间',
+    remark varchar(255) default null comment '备注',
+    primary key(id),
+    unique(monitor_code)
+) ENGINE=innodb DEFAULT CHARSET=utf8;
---
+++ src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="index">首页</div>
+</template>
+
+<script>
+  export default {
+    name: 'Index',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+</style>
